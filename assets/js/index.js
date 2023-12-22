bb1de55f43039ab05ddf4c3867eae94fbc07c011
yashrajput0508 <yasharajput0508@gmail.com>
2023-12-22 11:55:25 +0530

    final

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,55 +58,6 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener('scroll', updateActiveLink);
 });
 
-
-
-
-
-/* Carousal Video Script */
-
-// Load the YouTube IFrame Player API asynchronously
-// Load the YouTube IFrame Player API asynchronously
-// var tag = document.createElement('script');
-// tag.src = 'https://www.youtube.com/iframe_api';
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// var player;
-// function onYouTubeIframeAPIReady() {
-//     player = new YT.Player('player', {
-//         height: '711',
-//         width: '1264',
-//         top: '-103',
-//         videoId: 'Wg8-jmsgfiI', // Replace with your video ID
-//         frameborder: '0',
-//         playerVars: {
-//             'autoplay': 1,
-//             'controls': 0,
-//             'showinfo': 0,
-//             'mute': 1,
-//             'fs': 1,
-//             'modestbranding': 1, // Remove YouTube logo
-//             'iv_load_policy': 3, // Hide video annotations
-//             'disablekb': 1 // Disable keyboard controls
-//         },
-//         events: {
-//             'onReady': onPlayerReady,
-//             'onStateChange': onPlayerStateChange
-//         }
-//     });
-// }
-
-// function onPlayerReady(event) {
-//     event.target.playVideo(); // Autoplay the video once it's ready
-// }
-
-// function onPlayerStateChange(event) {
-//     if (event.data === YT.PlayerState.ENDED) {
-//         player.seekTo(0); // Rewind the video to the beginning when it ends
-//         player.playVideo(); // Play the video again
-//     }
-// }
-
 function visit() {
     const elementToScrollTo = document.getElementById('vision');
     elementToScrollTo.scrollIntoView({ behavior: 'smooth' });
